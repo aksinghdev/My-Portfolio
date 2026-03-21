cd90410f8ecb8993c994204acc9e6b16946d95b3
aksinghdev <krabhi1452@gmail.com>
2026-03-21 20:19:30 +0530

    fix env config in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
-dotenv.config({ path: "./server/.env" });
+// dotenv.config({ path: "./server/.env" });
+dotenv.config();
 
 import express from "express"
 import cors from "cors"
