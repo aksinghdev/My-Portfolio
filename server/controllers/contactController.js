c6b1c9f08952d5aa6ef28f2899a0eee4e4d8a339
aksinghdev <krabhi1452@gmail.com>
2026-03-25 19:23:08 +0530

    fix create in message model

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -7,13 +7,13 @@ export const sendMessage = async(req,res)=>{
     const {name,email,mobileNo,subject,message} = req.body
     console.log("print get data ",name,email,subject,message);
     try{
-        const person = await Message.create(
+        const person = await Message.create({
             name,
             email,
             mobileNo,
             subject,
             message,
-        )
+        })
         // const data = person; 
         return res.status(200).json({
             success : true,
