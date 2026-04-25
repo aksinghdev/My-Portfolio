41579caa6fe482f1d1a1941a6b36091b8beb9e0d
Abhishek Kumar Singh <krabhi1452@gmail.com>
2026-04-25 14:54:47 +0530

    fix import chat controller

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-import { chatWithAI } from "../controllers/chatControllers.js";
+import { chatWithAI } from "../controllers/chatController.js";
 
 const router = express.Router();
 
